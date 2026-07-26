6379de7b43e17cf68c4970320f55d0720bd4b239
Mikhail Zizganov <mzizganov@yandex.ru>
2026-07-26 12:29:24 +0000

    Исправил замечания наставника: добавил Nginx, Gunicorn, статику, обновил .gitignore и workflow

--- config/settings.py
+++ config/settings.py
@@ -85,9 +85,10 @@ TIME_ZONE = "Europe/Moscow"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_URL = '/static/'
+STATIC_ROOT = '/app/static/'
+
+
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
